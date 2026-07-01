8f2c45b2cd8178bdd53eeb9795a7d0ead6c7d3c3
malaksalim <malakalburtmani@gmail.com>
2026-07-01 14:28:43 +0400

    task1 info card

--- personal info card /Program.cs
+++ personal info card /Program.cs
@@ -4,11 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
+        // task1 personal info card 
         string name = "Sara";
         int age = 21;
         double height = 1.65;
         bool student = true;
         
         Console.Write("Name :" + name+", Age :"+age+", Height :"+ height+", Student :" +student  );
+        
+        
+        
     }
 }
--- personal info card /obj/Debug/net10.0/personal info card.AssemblyInfo.cs
+++ personal info card /obj/Debug/net10.0/personal info card.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personal info card")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3d06baae0771baaadae2c5c2ddc694427022673")]
 [assembly: System.Reflection.AssemblyProductAttribute("personal info card")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personal info card")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
