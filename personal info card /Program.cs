d4b66b9691a6e6d5ec272f67c960269b583ea3c1
malaksalim <malakalburtmani@gmail.com>
2026-07-01 16:11:08 +0400

    task9 day name finder

--- personal info card /Program.cs
+++ personal info card /Program.cs
@@ -158,6 +158,36 @@ class Program
         {
             Console.WriteLine("no discount applied  your bill is "+ billAmount+"OMR");
         }
+        // task9 day name finder 
+        Console.Write("enter a number (1-7) : ");
+        int day = int.Parse(Console.ReadLine());
 
+        switch (day)
+        { 
+            case 1 :
+                Console.WriteLine("Sunday ");
+                break;
+            case 2 :
+                Console.WriteLine("Monday ");
+                break;
+            case 3 :
+                Console.WriteLine("Tuesday ");
+                break;
+            case 4 :
+                Console.WriteLine("wednesday ");
+                break;
+            case 5 :
+                Console.WriteLine("Thursday ");
+                break;
+            case 6 :
+                Console.WriteLine("Friday ");
+                break;
+            case 7 :
+                Console.WriteLine("Saturday ");
+                break;
+            default:
+                Console.WriteLine("Invalid day number");
+                break;
+        }
     }
 }
