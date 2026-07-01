d3d06baae0771baaadae2c5c2ddc694427022673
malaksalim <malakalburtmani@gmail.com>
2026-07-01 11:14:02 +0400

    task1 personal info card "variables"

---
+++ personal info card /Program.cs
@@ -0,0 +1,14 @@
+﻿namespace personal_info_card;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = "Sara";
+        int age = 21;
+        double height = 1.65;
+        bool student = true;
+        
+        Console.Write("Name :" + name+", Age :"+age+", Height :"+ height+", Student :" +student  );
+    }
+}
